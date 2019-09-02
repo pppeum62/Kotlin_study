9f5e201bbc4ecb36a8b10d4b79a2dd6b11195847
pppeum <s2018s13@e-mirim.hs.kr>
2019-09-03 08:11:38 +0900

    first commit

---
+++ src/Hello.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+    println("Hello Kotlin")
+
+    var value = "str"
+    var value2 : String = "str"
+    var num = 100
+    var num2 : Int = 100
+
+    val num3 = 300 // 상수
+}
